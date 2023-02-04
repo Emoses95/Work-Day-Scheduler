9ac5358004e0a2df04aec5232710acb2f0984425
Emoses95 <moses.e.collins@gmail.com>
2023-02-04 09:42:32 -0800

    standard working hour blocks added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,44 @@
+var currentDay = $('#currentDay');
+var 
+
+// Adds event to local storage and prints the event data
+function handleEventFormSubmit(event){
+  event.preventDefault();
+
+  // read user input from the timeblock
+  var hour9= timeblockInputEl.val().trim();
+  var hour10= timeblockInputEl.val().trim();
+  var hour11= timeblockInputEl.val().trim(); 
+
+var newhour= {
+  timeblock1: hour9,
+  timeblock2: hour10,
+  timeblock3: hour11,
+};
+
+// Add timeblocks to local storage
+var timeblocks= readTimeblocksFromStorage();
+timeblocks.push(newTimeblock);
+saveTimeblockToStorage(timeblocks);
+
+// print project data
+printTimeblockData();
+
+// clear the form inputs
+timeblockInputEl.val('');
+}
+
+
+
+// Handles displaying the time
+function displyDay(){
+  var rightNow= dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  currentDay.text(rightNow);
+}
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-dayjs('mm,dd,yyyy')
-$(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -21,5 +57,5 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+  ;
   
